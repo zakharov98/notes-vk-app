712e4a4ac2e1768c1cc2f1c9759bb35cfe08ca0a
Zakharov98 <D.Zakharov98@yandex.ru>
2022-03-22 21:19:46 +0500

    add eruda

--- src/index.js
+++ src/index.js
@@ -15,6 +15,10 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+if (process.env.NODE_ENV === "development") {
+  import("./eruda").then(({ default: eruda }) => {}); //runtime download
+}
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
